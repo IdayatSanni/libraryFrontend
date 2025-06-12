81015974c82e16d39190db69916cb7ccfe01e6d1
Idayat Sanni <idayatsanni@gmail.com>
2025-06-12 18:09:00 -0400

    Vite React project with Tailwind

---
+++ library-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
+
+function App() {
+  return (
+    <Router>
+      <div className='max-w-4xl mx-auto p-4'>
+        <Routes>
+          <Route path='/' element={<BookList />} />
+          <Route path='/add' element={<BookForm />} />
+          <Route path='/edit/:id' element={<BookForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
